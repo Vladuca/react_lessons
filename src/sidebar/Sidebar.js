99245f9bbade5f3107cb4db09156aa94fc555ec2
Bogdan Dima <bogdan_dima@ukr.net>
2025-07-08 20:13:38 +0300

    add_sidebar

---
+++ src/sidebar/Sidebar.js
@@ -0,0 +1,12 @@
+import './Sidebar.css';
+
+function Sidebar(){
+    return(
+        <div className="Sidebar">
+            <nav></nav>
+            
+        </div>
+    )
+}
+
+export default Sidebar;
