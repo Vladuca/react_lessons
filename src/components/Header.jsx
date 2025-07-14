214a5ac0ea2c08906101426886b1887e35fc0a49
bogdan_dima <bogdan_dima@ukr.net>
2025-07-14 21:30:38 +0300

    41

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header(){
+    return(
+        <header>
+                <h3>Result univercity</h3>
+                <span>Тут будет время</span>
+            </header>
+    )
+}
