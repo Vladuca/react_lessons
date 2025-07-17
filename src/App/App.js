97bdd0a445a5f6da8eb558c704393f6da0b325f4
Bogdan Dima <bogdan_dima@ukr.net>
2025-07-17 21:32:46 +0300

    1.23.50

--- src/App/App.js
+++ src/App/App.js
@@ -2,11 +2,20 @@
 import Header from "../components/Header";
 import {ways} from './data';
 import WayToTeach from "../components/WayToTeach";
+import Button from "../components/Button/Button";
+import { useState } from "react";
 
 
 
 
 export default function App(){
+    const stateArray = useState('Нажми на кнопку');
+    
+
+    let content = 'Нажми на кнопку';
+    function handeClick(type){
+        content = type;   
+    }
     return(
         <div>
             <Header />
@@ -18,8 +27,12 @@ export default function App(){
                         <WayToTeach {...ways[1]}/>
                     </ul>
                 </section>
+                <Button onClick={()=>handeClick(ways[0].discription)}>Подход</Button>
+                <Button onClick={()=>handeClick('easy')}>Доступность</Button>
+                <Button onClick={()=>handeClick('program')}>Концентрация</Button>
+                
             </main>
-
+            <p>{content}</p>
         </div>
     )
 }
